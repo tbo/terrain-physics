966a7949a510bcbb715b44c330d65576dde33aa4
Thomas Bonk <tbo@cybo.biz>
2015-04-28 00:34:46 +0200

    Bumped versions and added json loader

--- webpack.config.js
+++ webpack.config.js
@@ -12,7 +12,8 @@ module.exports = {
     module: {
         loaders: [
             {test: /\.js$/, exclude: /node_modules/, loader: 'babel-loader'},
-            {test: /\.js$/, loader: "eslint-loader", exclude: /node_modules/}
+            {test: /\.js$/, loader: "eslint-loader", exclude: /node_modules/},
+            {test: /\.json$/, loader: 'json-loader'}
         ]
     }
 }
